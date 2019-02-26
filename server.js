b8f336af2c8e9912622becde2d52a66644aee67a
Chaitanya Baranwal <chaitanyabaranwal@gmail.com>
2019-02-26 18:41:26 +0800

    Update Port

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/', function(request, response) {
   response.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(3002, function() {
-    console.log('Listening on port 3002.');
+app.listen(process.env.PORT || 3002, function() {
+    console.log('Listening on port!');
 });
